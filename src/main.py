1341d601fc43ac66545db0e3cb337e171989b48c
EvKon-MSQ <mainsidequest@gmail.com>
2025-01-08 21:51:31 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+from textnode import TextType
+
+def main():
+    test = TextNode("this is a test", TextType.BOLD_TEXT, "https://test.com")
+    print (test)
+
+main()
+
+
