6c172c138bf0d0414067c45fdb320ca510200541
EvKon-MSQ <mainsidequest@gmail.com>
2025-01-21 18:41:10 +0200

    Added textnode to html node function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
-import unittest
+import unittest, pytest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +28,54 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD_TEXT, "http://git.com")
         node2 = TextNode("This is a text node", TextType.BOLD_TEXT, "http://git.org")
         self.assertNotEqual(node, node2)
+
+    def test_text_node_to_html_node(self):
+        #tests normal text, no tags
+        node = TextNode("Just a plain text", TextType.NORMAL_TEXT)
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag == None
+        assert html_node.value == "Just a plain text"
+        assert html_node.props == None
+
+        #tests bold text and tag
+        node = TextNode("Just a bold text", TextType.BOLD_TEXT)
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag == "b"
+        assert html_node.value == "Just a bold text"
+        assert html_node.props == None
+
+        #tests italic text and tag
+        node = TextNode("Just an italic text", TextType.ITALIC_TEXT)
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag == "i"
+        assert html_node.value == "Just an italic text"
+        assert html_node.props == None
+
+        #tests code text and tag
+        node = TextNode("Just a code text", TextType.CODE_TEXT)
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag == "code"
+        assert html_node.value == "Just a code text"
+        assert html_node.props == None
+
+        #tests link tag, text and prop dictionary
+        node = TextNode("Just a link text", TextType.LINK, "www.test.com")
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag == "a"
+        assert html_node.value == "Just a link text"
+        assert html_node.props == {"href": "www.test.com"}
+        
+        #tests img tag, if value is empty string and props dictionary
+        node = TextNode("Just an img text", TextType.IMAGE, "www.test.com")
+        html_node = text_node_to_html_node(node)
+        assert html_node.tag == "img"
+        assert html_node.value == ""
+        assert html_node.props == {"src": "www.test.com", "alt": "Just an img text"}
+
+        #tests exception when text type is wrong
+        with pytest.raises(Exception):
+            node = TextNode("Just a plain text", TextType.TEST_TYPE)
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     NORMAL_TEXT = "normal"
@@ -7,6 +8,9 @@ class TextType(Enum):
     CODE_TEXT= "code"
     LINK = "link"
     IMAGE = "image"
+
+    #For Test only!
+    TEST_TYPE = "test"
 
 class TextNode:
     def __init__(self, text, text_type, url = None):
@@ -21,3 +25,23 @@ class TextNode:
     
     def __repr__(self):
         return  f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.NORMAL_TEXT:
+        return LeafNode(tag = None, value = text_node.text)
+    elif text_node.text_type == TextType.BOLD_TEXT:
+        return LeafNode(tag = "b", value = text_node.text)
+    elif text_node.text_type == TextType.ITALIC_TEXT:
+        return LeafNode(tag = "i", value = text_node.text)
+    elif text_node.text_type == TextType.CODE_TEXT:
+        return LeafNode(tag = "code", value = text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode(tag = "a", value = text_node.text, props = {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode(
+            tag = "img", 
+            value = "", 
+            props = {"src": text_node.url, "alt": text_node.text}
+        ) 
+    else:
+        raise Exception ("Wrong text type!")
